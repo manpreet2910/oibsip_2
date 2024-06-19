fe34b5d871c0ea80e9536f266ba24540f699a294
manpreet2910 <manu2910.sm@gmail.com>
2024-06-19 15:04:18 +0530

    code changes

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,33 +1,31 @@
-def getWeight():
+def Weight_input():
     weight = float(input("Please enter your weight in kg: "))
     return weight
 
-def getHeight():
+def Height_input():
     height = input("Please enter your height in feet and inches (e.g. 5'11): ")
-    splitString = height.split("'")
-    feet = float(splitString[0])
-    inches = float(splitString[1].strip('"'))
+    split_string = height.split("'")
+    feet = float(split_string[0])
+    inches = float(split_string[1].strip('"'))
     total_inches = (feet * 12) + inches
     return total_inches
 
-def calculateBMI(weight, height):
-    # Convert height (inches) to meters
-    meterHeight = height * 0.0254
-    # Calculate BMI
-    bmi = weight / (meterHeight ** 2)
+def calculate_BMI(weight, height):
+    meter_height = height * 0.0254
+    bmi = weight / (meter_height ** 2)
     return bmi
 
 if __name__ == "__main__":
     print(" Welcome to Manpreet's BMI Calculator ! ")
 
-    weight = getWeight()
-    height = getHeight()
+    weight = Weight_input()
+    height = Height_input()
 
-    bmi = calculateBMI(weight, height)
+    bmi = calculate_BMI(weight, height)
     print(f"Your final BMI is: {round(bmi, 2)}")
     if bmi < 18.5:
-        print("You are underweight!")
+        print("You are underweight !")
     elif 18.5 <= bmi < 24.9:
-        print("You are normal weight.")
+        print("You are Fit !")
     elif bmi >= 25:
         print("You are overweight!")
